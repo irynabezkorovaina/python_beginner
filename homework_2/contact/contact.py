a5b3d7d88dea0bfe4b9577fa30d1b1009ad1e3ce
irynabezkorovaina <iryna.bezkorovaina@gmail.com>
2024-05-17 01:53:21 +0300

    contact (task 14)

---
+++ homework_2/contact/contact.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+address = input("Enter your addres: ")
+phone = input("Enter your phone number: ")
+contact = {
+    'name': name,
+    'age': age,
+    'address': address,
+    'phone': phone
+}
+print('Contact created:', contact)
