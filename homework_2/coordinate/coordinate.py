b94947db4ebf972854875ec098da825e393b31d5
irynabezkorovaina <iryna.bezkorovaina@gmail.com>
2024-05-17 01:45:13 +0300

    coordinate (task 13)

---
+++ homework_2/coordinate/coordinate.py
@@ -0,0 +1,5 @@
+x = input("Enter the x coordinate: ")
+y = input("Enter the y coordinate: ")
+z = input("Enter the z coordinate: ")
+coordinate = f'x: {x}, y: {y}, z: {z}'
+print('Coordinate:', coordinate)
